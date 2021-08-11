8e6c3356b272dbc02194a35f7b68976a9c0aad32
DavorJ94 <davorjovanovic94@gmail.com>
2021-08-11 11:26:58 +0200

    Finished about pages

--- pages/about/[id].js
+++ pages/about/[id].js
@@ -1,3 +1,5 @@
+import styles from "../../styles/id.module.css";
+
 export const getStaticPaths = async () => {
   const response = await fetch(
     "https://gist.githubusercontent.com/DavorJ94/2f07667b4260a3c179005eb42be92f6f/raw/5124650c1ac780e2b7de1391fe228c65b339bcc8/pokemon-data.json"
@@ -30,47 +32,85 @@ export const getStaticProps = async (context) => {
 const AboutPokemon = ({ pokemon }) => {
   return (
     <div key={pokemon.id} style={{ textAlign: "center" }}>
-      <div>{pokemon.name}</div>
-      <div>Abilities: </div>
+      <img
+        alt="Pokemon img"
+        src={pokemon.sprites.front_shiny}
+        className={styles.pokemonImg}
+      ></img>
+      <div className={styles.pokemonName}>{pokemon.name}</div>
+      <div className={styles.leftSide}>Abilities: </div>
       {pokemon.abilities.map((ability, index) => {
         if (index < pokemon.abilities.length - 1)
-          return <span key={`ability-${index}`}>{ability.ability.name}, </span>;
+          return (
+            <span className={styles.abilities} key={`ability-${index}`}>
+              {ability.ability.name},{" "}
+            </span>
+          );
         else
-          return <span key={`ability-${index}`}>{ability.ability.name}. </span>;
+          return (
+            <span className={styles.abilities} key={`ability-${index}`}>
+              {ability.ability.name}.{" "}
+            </span>
+          );
       })}
-      <div>Height: {pokemon.height}</div>
-      <div>
+      <div className={styles.heightWeight}>
+        Height: <span className={styles.abilities}>{pokemon.height}</span>
+      </div>
+      <div className={styles.heightWeight}>
+        Weight: <span className={styles.abilities}>{pokemon.weight}</span>
+      </div>
+      <div
+        className={styles.heightWeight}
+        style={{ width: "80%", margin: "0 auto" }}
+      >
         {pokemon.moves.length >= 10
           ? "First 10 moves: "
           : `Its ${pokemon.moves.length} moves: `}
         {pokemon.moves.map((move, index) => {
-          if (index < pokemon.moves.length - 1)
-            return <span key={`move-${index}`}>{move.move.name}, </span>;
+          if (index < 9)
+            return (
+              <span className={styles.abilities} key={`move-${index}`}>
+                {move.move.name},{" "}
+              </span>
+            );
           else if (index === 9 || index === pokemon.moves.length - 1)
-            return <span key={`move-${index}`}>{move.move.name}. </span>;
+            return (
+              <span className={styles.abilities} key={`move-${index}`}>
+                {move.move.name}.{" "}
+              </span>
+            );
           else return;
         })}
       </div>
-      <img alt="Pokemon img" src={pokemon.sprites.front_shiny}></img>
       <div>
-        Status:
         {pokemon.stats.map((status, index) => {
           return (
-            <div key={`stats-${index}`}>
-              {status.stat.name}: <span> {status.base_stat}</span>
+            <div key={`stats-${index}`} className={styles.heightWeight}>
+              {status.stat.name}:{" "}
+              <span className={styles.abilities}> {status.base_stat}</span>
             </div>
           );
         })}
       </div>
       <div>
-        Types:
+        <span className={styles.heightWeight}>Types:</span>
         {pokemon.types.map((type, index) => {
           if (index < pokemon.types.length - 1)
-            return <span key={`types-${index}`}> {type.type.name},</span>;
-          else return <span key={`types-${index}`}> {type.type.name}.</span>;
+            return (
+              <span className={styles.abilities} key={`types-${index}`}>
+                {" "}
+                {type.type.name},
+              </span>
+            );
+          else
+            return (
+              <span className={styles.abilities} key={`types-${index}`}>
+                {" "}
+                {type.type.name}.
+              </span>
+            );
         })}
       </div>
-      <div>Weight: {pokemon.weight}</div>
     </div>
   );
 };
